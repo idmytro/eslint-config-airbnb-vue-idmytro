b3ee4c02e66af5754689c7ec1512d08831932f36
Dmytro Cisar <dmytro.cisar@gmail.com>
2022-01-25 19:30:35 +0200

    add vue/first-attribute-linebreak rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,71 +11,86 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 11,
   },
-  plugins: [
-    'vue',
-    'promise',
-  ],
+  plugins: ['vue', 'promise'],
   rules: {
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'always-multiline',
-    }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
     'newline-per-chained-call': [2, { ignoreChainWithDepth: 1 }],
     'no-prototype-builtins': 'off',
     'prefer-template': 2,
-    'vue/component-name-in-template-casing': ['error', 'kebab-case', {
-      registeredComponentsOnly: true,
-      ignores: [],
-    }],
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'never',
-        normal: 'never',
-        component: 'never',
+
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      {
+        registeredComponentsOnly: true,
+        ignores: [],
       },
-      svg: 'always',
-      math: 'always',
-    }],
-    'vue/order-in-components': ['error', {
-      order: [
-        'el',
-        'name',
-        'parent',
-        'functional',
-        ['delimiters', 'comments'],
-        ['template', 'render'],
-        'head',
-        'renderError',
-        ['components', 'directives', 'filters'],
-        'extends',
-        'mixins',
-        'inheritAttrs',
-        'model',
-        ['props', 'propsData'],
-        'fetch',
+    ],
+    'vue/first-attribute-linebreak': [
+      'error',
+      {
+        singleline: 'beside',
+      },
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'never',
+          component: 'never',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/order-in-components': [
+      'error',
+      {
+        order: [
+          'el',
+          'name',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['template', 'render'],
+          'head',
+          'renderError',
+          ['components', 'directives', 'filters'],
+          'extends',
+          'mixins',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'fetch',
 
-        'emits', // for Vue.js 3.x
-        'setup', // for Vue 3.x
+          'emits', // Vue 3
+          'setup', // Vue 3
 
-        'asyncData',
-        'data',
-        'computed',
-        'watch',
-        'LIFECYCLE_HOOKS',
-        'methods',
-      ],
-    }],
-    'vue/no-unused-properties': ['error', {
-      groups: [
-        'props',
-        'data',
-        'computed',
-        'methods',
-      ],
-    }],
+          'asyncData',
+          'data',
+          'computed',
+          'watch',
+          'LIFECYCLE_HOOKS',
+          'methods',
+        ],
+      },
+    ],
+    'vue/no-unused-properties': [
+      'error',
+      {
+        groups: ['props', 'data', 'computed', 'methods'],
+      },
+    ],
     'vue/no-v-html': 'off',
     'vue/prefer-template': 2,
   },
