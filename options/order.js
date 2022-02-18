160e9b61dc3ff8ddab1fcdcaf676a7eceb2f0191
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-02-18 21:02:29 +0200

    update eslint config

---
+++ options/order.js
@@ -0,0 +1,27 @@
+module.exports = [
+  'el',
+  'name',
+  'parent',
+  'functional',
+  ['delimiters', 'comments'],
+  ['template', 'render'],
+  'head',
+  'renderError',
+  ['components', 'directives', 'filters'],
+  'extends',
+  'mixins',
+  'inheritAttrs',
+  'model',
+  ['props', 'propsData'],
+  'fetch',
+
+  'emits', // Vue 3
+  'setup', // Vue 3
+
+  'asyncData',
+  'data',
+  'computed',
+  'watch',
+  'LIFECYCLE_HOOKS',
+  'methods',
+];
